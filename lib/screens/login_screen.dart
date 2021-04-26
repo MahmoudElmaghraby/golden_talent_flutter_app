dda03883299979d1552a1c121ccda00b6bbef5e7
elmaghraby515 <mahmoudelmaghraby515@gmail.com>
2021-04-26 23:07:54 +0200

    finsh login and register screens

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,31 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:golden_talent/screens/register_screen.dart';
+
+import 'home_screen.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width; //360
-    final screenHeight = MediaQuery.of(context).size.height; //760
+    final _screenWidth = MediaQuery.of(context).size.width; //360
+    final _screenHeight = MediaQuery.of(context).size.height; //760
 
     return Scaffold(
       backgroundColor: Color.fromRGBO(242, 242, 242, 1),
-      body: SingleChildScrollView( //Test commit
-        padding: EdgeInsets.only(top: screenHeight * 0.1),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.only(top: _screenHeight * 0.1),
         child: Column(
           children: [
             Image.asset(
               'assets/images/small_logo.png',
-              width: screenWidth * 0.5,
-              height: screenHeight * 0.26,
+              width: _screenWidth * 0.5,
+              height: _screenHeight * 0.26,
             ),
             Card(
               elevation: 5,
               color: Colors.white,
               child: Padding(
                 padding: EdgeInsets.only(
-                  left: screenWidth * 0.02,
-                  right: screenWidth * 0.02,
-                  top: screenHeight * 0.05,
-                  bottom: screenHeight * 0.05,
+                  left: _screenWidth * 0.02,
+                  right: _screenWidth * 0.02,
+                  top: _screenHeight * 0.05,
+                  bottom: _screenHeight * 0.05,
                 ),
                 child: Column(
                   children: [
@@ -35,7 +38,7 @@ class LoginScreen extends StatelessWidget {
                         labelText: 'Email or Username',
                       ),
                     ),
-                    SizedBox(height: screenHeight * 0.0211), //16
+                    SizedBox(height: _screenHeight * 0.0211), //16
                     TextField(
                       obscureText: true,
                       decoration: InputDecoration(
@@ -43,17 +46,23 @@ class LoginScreen extends StatelessWidget {
                         labelText: 'Password',
                       ),
                     ),
-                    SizedBox(height: screenHeight * 0.0105), //8
+                    SizedBox(height: _screenHeight * 0.0105), //8
                     ElevatedButton(
                       style: ElevatedButton.styleFrom(
                         primary: Color.fromRGBO(227, 58, 61, 1),
                         padding:
-                            EdgeInsets.symmetric(horizontal: screenWidth * 0.2),
+                            EdgeInsets.symmetric(horizontal: _screenWidth * 0.2),
                       ),
                       child: Text('Log in'),
-                      onPressed: () {},
+                      onPressed: () {
+                        return Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (_) => HomeScreen(),
+                          ),
+                        );
+                      },
                     ),
-                    SizedBox(height: screenHeight * 0.0105), //8
+                    SizedBox(height: _screenHeight * 0.0105), //8
                     Text(
                       'Forget password?',
                       style: TextStyle(
@@ -61,7 +70,7 @@ class LoginScreen extends StatelessWidget {
                         color: Colors.grey,
                       ),
                     ),
-                    SizedBox(height: screenHeight * 0.0105), //8
+                    SizedBox(height: _screenHeight * 0.0105), //8
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
@@ -69,15 +78,24 @@ class LoginScreen extends StatelessWidget {
                           'Don\'t have an account?',
                           style: TextStyle(fontSize: 18, color: Colors.black),
                         ),
-                        SizedBox(width: screenHeight * 0.0105), //8
-                        Text(
-                          'Sign in',
-                          style:
-                              TextStyle(fontSize: 18, color: Colors.lightBlue),
+                        SizedBox(width: _screenHeight * 0.0105), //8
+                        GestureDetector(
+                          onTap: () {
+                            return Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (_) => RegisterScreen(),
+                              ),
+                            );
+                          },
+                          child: Text(
+                            'Sign in',
+                            style: TextStyle(
+                                fontSize: 18, color: Colors.lightBlue),
+                          ),
                         ),
                       ],
                     ),
-                    SizedBox(height: screenHeight * 0.0105), //8
+                    SizedBox(height: _screenHeight * 0.0105), //8
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
---
+++ lib/screens/register_screen.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:golden_talent/screens/home_screen.dart';
+
+class RegisterScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final _screenWidth = MediaQuery.of(context).size.width; //360
+    final _screenHeight = MediaQuery.of(context).size.height; //760
+
+    var _birthdayController = TextEditingController();
+
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(242, 242, 242, 1),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.only(top: _screenHeight * 0.05),
+        child: Column(
+          children: [
+            Card(
+              elevation: 5,
+              color: Colors.white,
+              child: Padding(
+                padding: EdgeInsets.only(
+                  left: _screenWidth * 0.02,
+                  right: _screenWidth * 0.02,
+                  top: _screenHeight * 0.05,
+                  bottom: _screenHeight * 0.05,
+                ),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'First Name',
+                      ),
+                    ),
+                    SizedBox(height: _screenHeight * 0.0211), //16
+                    TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Last Name',
+                      ),
+                    ),
+                    SizedBox(height: _screenHeight * 0.0211), //16
+                    TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Username',
+                      ),
+                    ),
+
+                    SizedBox(height: _screenHeight * 0.0211), //16
+                    TextField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Email',
+                      ),
+                    ),
+                    SizedBox(height: _screenHeight * 0.0211), //16
+                    TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Personal ID (Optional)',
+                      ),
+                    ),
+                    SizedBox(height: _screenHeight * 0.0211), //16
+                    TextField(
+                      controller: _birthdayController,
+                      onTap: () {
+                        DatePicker.showDatePicker(
+                          context,
+                          showTitleActions: true,
+                          minTime: DateTime(1950, 3, 5),
+                          maxTime: DateTime(2015, 6, 7),
+                          onConfirm: (date) {
+                            _birthdayController.text =
+                                '${date.year} - ${date.month} - ${date.day}';
+                          },
+                          currentTime: DateTime.now(),
+                          locale: LocaleType.en,
+                        );
+                      },
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Birthday',
+                      ),
+                    ),
+                    SizedBox(height: _screenHeight * 0.0211), //16
+                    TextField(
+                      keyboardType: TextInputType.phone,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Phone Number',
+                      ),
+                    ),
+                    SizedBox(height: _screenHeight * 0.0211), //16
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Password',
+                      ),
+                    ),
+                    SizedBox(height: _screenHeight * 0.0211), //16
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Re-Password',
+                      ),
+                    ),
+                    SizedBox(height: _screenHeight * 0.0105), //8
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Color.fromRGBO(227, 58, 61, 1),
+                        padding: EdgeInsets.symmetric(
+                            horizontal: _screenWidth * 0.2),
+                      ),
+                      child: Text('Sign up'),
+                      onPressed: () {
+                        return Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (_) => HomeScreen(),
+                          ),
+                        );
+                      },
+                    ),
+                    SizedBox(height: _screenHeight * 0.0105), //8
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
